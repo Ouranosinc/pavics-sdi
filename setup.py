84d4b92a45d3a7a9d22885f7322f9071f2636132
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2020-12-02 13:13:40 -0500

    Abandon conda and use image python and pip instead

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 CLASSIFIERS = [
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python',
-    'Operating System :: OS Independent',
-    'Topic :: Scientific/Engineering :: Atmospheric Science',
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python",
+    "Operating System :: OS Independent",
+    "Topic :: Scientific/Engineering :: Atmospheric Science",
 ]
 
 setup(
@@ -13,10 +13,21 @@ setup(
     version="0.1",
     url="https://pavics-sdi.readthedocs.io",
     install_requires=[],
+    extras_require={
+        "docs": [
+            "sphinx>=1.4",
+            "sphinx-intl",
+            "sphinx_rtd_theme",
+            "sphinx-jsondomain",
+            "sphinx-jsonschema",
+            "nbsphinx",
+            "jupyter",
+        ]
+    },
     author="CRIM/Ouranos",
     author_email="pavics@ouranos.ca",
     description="Scientific gateway for climate data analytics.",
-    long_description=open('README.rst', 'rt').read(),
+    long_description=open("README.rst", "rt").read(),
     license="BSD",
     classifiers=CLASSIFIERS,
 )
