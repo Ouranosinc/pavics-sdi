0e793c033980d340742ed39b46f3895b960c91f9
tlogan2000 <logan.travis@ouranos.ca>
2026-04-13 09:56:56 -0400

    exclude from docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -156,6 +156,8 @@ exclude_patterns = [
     "processes/searching.rst",
     "processes/workflows.rst",
     "tutorials/searching.rst",
+    # PAVICS training workshop materials
+    "notebooks/PAVICS-training/*.ipynb"
 ]
 
 # The reST default role (used for this markup: `text`) to use for all
